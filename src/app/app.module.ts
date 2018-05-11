767f2db1f4abb6b5c94c54ed367817c71903c963
Cristofer Herrera <ckriss18@gmail.com>
2018-05-10 23:52:51 -0300

    se comienza a crear la estructura del proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { VerticalNavBarComponent } from './vertical-nav-bar/vertical-nav-bar.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { CenterContentComponent } from './center-content/center-content.component';
+import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VerticalNavBarComponent,
+    TopBarComponent,
+    CenterContentComponent,
+    BreadcrumbsComponent
   ],
   imports: [
     BrowserModule
