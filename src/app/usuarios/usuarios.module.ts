0913905894978ce1460881cc1ea3fa430fe146b4
Cristofer Herrera <ckriss18@gmail.com>
2018-05-12 23:55:25 -0300

    se añade clase de serviocs

--- src/app/usuarios/usuarios.module.ts
+++ src/app/usuarios/usuarios.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListUsuariosComponent } from './list-usuarios/list-usuarios.component';
 import { RouterModule, Routes } from '@angular/router';
+import { UsuariosService } from './usuarios.service';
 
 const appRoutes: Routes = [
   { path: 'usuarios', component: ListUsuariosComponent }
@@ -20,7 +21,8 @@ const appRoutes: Routes = [
   ],
   exports: [
     ListUsuariosComponent
-  ]
+  ],
+  providers: [UsuariosService]
 
 })
 export class UsuariosModule { }
